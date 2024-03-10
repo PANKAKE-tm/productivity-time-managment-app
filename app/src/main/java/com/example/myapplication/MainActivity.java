aede40d4838b92fb61ce296dd0a43159b090a1ef
Arnelys <arnas1373@gmail.com>
2024-03-10 15:39:23 +0200

    Fixing warnings, allowing easier access to get TimerPhase for setting notifications

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,28 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button pomodoroButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pomodoroButt = findViewById(R.id.startButton);
-        pomodoroButt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), PomodoroTimer.class);
-                intent.putExtra("StudyTime", (long)(0.1 * 60 * 1000));
-                intent.putExtra("RestTime", (long)(0.12 * 60 * 1000));
+        Button pomodoroButt = findViewById(R.id.startButton);
+        pomodoroButt.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), PomodoroTimer.class);
+            intent.putExtra("StudyTime", (long)(1 * 60 * 1000));
+            intent.putExtra("RestTime", (long)(0.5 * 60 * 1000));
 
-                startActivity(intent);
-            }
+            startActivity(intent);
         });
     }
 }
