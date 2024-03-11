c1b83cf8ccea6ff590df4aa23da6416aa41c799b
aurimassG <119803436+aurimassG@users.noreply.github.com>
2024-03-11 17:33:34 +0200

    Created nav menu with navigations

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,65 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        Button pomodoroButt = findViewById(R.id.startButton);
-        pomodoroButt.setOnClickListener(v -> {
-            Intent intent = new Intent(getBaseContext(), PomodoroTimer.class);
-            intent.putExtra("StudyTime", (long)(1 * 60 * 1000));
-            intent.putExtra("RestTime", (long)(0.5 * 60 * 1000));
+                int itemId = item.getItemId();
 
-            startActivity(intent);
+                if (itemId == R.id.navHome)
+                {
+                    loadFragment(new HomeFragment(), false);
+                } else if (itemId == R.id.navNews) {
+                    loadFragment(new NewsFragment(), false);
+                } else if (itemId == R.id.navSettings) {
+                    loadFragment(new SettingsFragment(), false);
+                } else if (itemId == R.id.navStatistics) {
+                    loadFragment(new StatisticsFragment(), false);
+                } else { // nav Tasks
+                    loadFragment(new TasksFragment(), false);
+                }
+                return true;
+            }
         });
+
+        bottomNavigationView.setSelectedItemId(R.id.navHome);
+        loadFragment(new HomeFragment(), true);
+    }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.commit();
     }
 }
